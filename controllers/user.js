ab3bf7ec017ef48a571336cb278991212bcb27aa
Saptorsi Ghose Dastidar <saptorsighosedastidarvis@gmail.com>
2025-07-21 23:30:47 +0530

    jwt token added

--- controllers/user.js
+++ controllers/user.js
@@ -15,16 +15,15 @@ async function handleUserSignup(req, res){
 
 async function handleUserLogin(req, res){
     const { email, password } = req.body;
-    const user = User.findOne({email, password});
+    const user = await User.findOne({email, password});
     if(!user){
         return res.render("login", {
             error: "Invalid email or password",
         });
     }
 
-    const sessionId = uuidv4();
-    setUser(sessionId, user);
-    res.cookie("uid", sessionId);
+    const token = setUser(user);
+    res.cookie("uid", token);
     return res.redirect("/");
 }
 
--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,22 @@
-const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken");
+const secret = "Saptorsi@123$";
 
-function setUser(id, user){
-    sessionIdToUserMap.set(id, user); 
+function setUser(user){
+    console.log("Creating token for user:", user);
+    return jwt.sign({
+        _id: user._id,
+        email: user.email,
+    }, secret);
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
+function getUser(token){
+    if(!token) return null;
+    try {
+        return jwt.verify(token, secret);
+    } catch (error) {
+        return null;
+    }
+    
 }
 
 module.exports = {
